3b14e363d7ce80f5cac63cadfc246a758f29fa8d
On_Chain_Yogi <golujugairar@gmail.com>
2025-11-12 16:18:42 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,21 @@
+#Question1- Write a Python program to take a user's name as input and print a greeting
+#message with print().
+
+name=str(input("Please write your name here"))
+print("Hello",name)
+print("we will create a message for you")
+print("Hello",name)
+mood=str(input("How are you doing?"))
+activities=str(input("How are your day being spend?"))
+goodbye=str(input("Okay then BYE take care"))
+print("\n ---------------------------------------------- \n")
+
+#Question2 Take two integer inputs from the user and print their sum, difference,product and quotient.
+a=int(input("Please write your birthday date"))
+b=int(input("Please write IN which year you born"))
+print("Addition=",a+b)
+print("Subtraction=",a-b)
+print("Multiplication=",a*b)
+print("division=",a/b)
+print("So these are the sum,difference, product, and quotient of your DOB")
+print("\n ---------------------------------------------- \n")
